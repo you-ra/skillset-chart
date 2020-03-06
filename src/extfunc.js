3260e96ebdc70dcd933d2820f0f315fb78407f40
betejur <y.betehtin@gmail.com>
2020-03-06 22:23:55 +0200

    third yellow commit

--- src/extfunc.js
+++ src/extfunc.js
@@ -1,6 +1,23 @@
 
 module.exports = {
 
+	scaleCol : function (p, bsize) {
+		var w = p.width;
+		var curColnum = p.floor(w/bsize);
+		//var incColwidth = w/curColnum-1;
+		//var decColwidth = w/curColnum+1;
+		var mBsize = p.floor(bsize*mul);
+
+		if(mBsize > bsize && mBsize * (curColnum-1) >= w){
+			return {w : mBsize, colnum : curColnum-1, r: true};
+		}
+
+		if(mBsize < bsize && mBsize * (curColnum+1) <= w){
+			return {w : mBsize, colnum : curColnum+1, r : true};
+		}
+
+	},
+
 	a : function (p){
 
 		img = [];
@@ -13,12 +30,72 @@ module.exports = {
 		
 	},
 
-	b : function (p){
-	
-		var x = 100;
-		var y = 200;
-		p.ellipse(x,y,50,50);
+	setBp : function (p){
+		const brPoints =[900,600,200];
+		var bp = {colWidth : 0, colNum : 5};
+		bp.colWidth = p.width/5;
+
+		if(p.width < brPoints[0] && p.width > brPoints[1]){
+			console.log('5');
+		};
+
+		if(p.width < brPoints[1] && p.width > brPoints[2]){
+			console.log('4');
+			bp.colWidth = p.width/4;
+			bp.colNum = 4;
+		};
+
+		if(p.width < brPoints[2]){
+			console.log('3');
+			bp.colWidth = p.width/3;
+			bp.colNum = 3;
+		};
+		console.log(bp.colWidth);
+		return bp;
+	},
+
+	Slider : function (p, name, position, range, absValue){
+		let h = 100;
+		let margins = 60;
+		//let width = p.windowWidth-margins*2;
+		this.name = name;
+		this.position = position;
+		this.baseline = 600-(this.position*h-h/2);
+		this.cursorX = p.width*absValue;
+		this.active = false;
+
 		
+		this.display = function(){
+
+			p.rect(0, this.baseline-h/2, p.width, this.baseline+h/2);
+			p.text(this.name +' '+ this.cursorX, p.width/2, this.baseline - 30);
+			p.line(margins, this.baseline, p.width - margins, this.baseline);
+			p.circle(this.cursorX, this.baseline, 20);
+			
+		};
+
+		this.setCursorX = function(mousex){
+			if(this.active){
+				if(mousex >= margins && mousex <= p.width - margins){
+					this.cursorX = mousex;
+				}else{
+					if(mousex < margins){
+						this.cursorX = margins;
+					}else{this.cursorX = p.width - margins};
+				}
+			}
+			//this.cursorX = p.map(mousex, 0, p.width, margins, p.width-margins, true);
+		}
+
+		this.getValue = function(from, to){
+			return p.map(this.cursorX, margins, p.width+margins, from, to);
+		}
+
+		this.hit = function(mousey){
+			if(mousey>p.height-h*position && mousey<p.height-h*(position-1)){
+				this.active = true;
+			}
+		}
 	}
 	
 };
--- src/sketch2.js
+++ src/sketch2.js
@@ -8,7 +8,11 @@ new p5(function(p){
 	const size = [3,6,9,12,15,18,21,24];
 	var img;
 	var pos = 30;
+	var BP;
+	var boxSize;
+	var scale = null;
 	//var gui;
+
 	p.preload = function(){
 		img = extfunc.a(p);
 	}
@@ -17,34 +21,87 @@ new p5(function(p){
 		
 		//img[0].loadPixels();
 	 	p.createCanvas(p.windowWidth, 600);
+	 	
 	 	mask = p.createGraphics(50, 50);
     	mask.ellipse(25,25,48,48);
+		img[0].mask(mask);
 		img[1].mask(mask);
+		BP = extfunc.setBp(p);
+		boxSize = BP.colWidth;
 		//gui = createGui('mygui');
 		//gui.addGlobals('pos');
 		p.noLoop();
+		s1 = new extfunc.Slider(p, 'Box size', 1, [1,50], 0.5);
+		s2 = new extfunc.Slider(p, 'Circle size', 2,[1,10], 0.7);
 		
 
 		
 	};
 
 	p.draw = function() {
-		//if(!done){
+		
 
-			p.background(201, 15, 50);
-			p.image(img[1], 25, 25, 50, 50);
-			for (i = 0; i<name.length; i++){
-				s = p.random(size);
-				p.text(name[i], 50, 70*(i+1));
-				p.ellipse(100+pos,70*(i+1)-4,s,s);
-				//if (i<name.length) done = true;
-			} 
-			
-		//}
+				p.background(0, 100, 150);
+				//scale = extfunc.scaleCol(p, boxSize, s1.getValue(0.5, 1.5));
+				if(scale){
+					boxSize = scale.w;
+					console.log(scale, BP.colWidth);
+					console.log(s1.getValue(0.5, 1.5));
+				}
+
+				for (var i = 0; i < BP.colNum; i++){
+					
+					p.image(img[i%2], i*BP.colWidth*s1.getValue(0.5,1.5), 25, 50, 50);
+					for (n = 0; n < skillset.length; n++){
+						//p.rect(n*BP.colWidth,i*200,BP.colWidth,200);
+
+						
+						s = p.random(size);
+						//p.text(name[i], 50, 70*(i+1));
+						p.ellipse(100+pos,70*(i+1)-4,s,s);
+					}
+				} 
+				
+				
+				
+	  			//boxSize = p.round(BP.colWidth * s1.getValue(0.5, 1.5));
+	  			p.text(boxSize,100,100);
+	  			
+	  			
+	  			
+	  			s1.setCursorX(p.mouseX);
+	  			s1.display();
+		
 	};
 
 	p.windowResized = function() {
+		
 		p.resizeCanvas(p.windowWidth, 600);
+		BP = extfunc.setBp(p);
+		
 	};
+
+	p.mouseDragged = function(event) {
+		
+  		//p.ellipse(p.mouseX,p.mouseY,50);
+  		
+  		//p.loop();
+
+  		return false;
+	}
+
+	p.mousePressed = function(){
+		//s1.setCursorX(p.mouseX);
+  		//s1.display();
+  		s1.hit(p.mouseY);
+  		p.loop();
+	}
+
+	p.mouseReleased = function(){
+		s1.active = false;
+		p.noLoop();
+	}
+
+
 
 }, 'root');
